8b509d9f8eeee8da6a3a517c1842ba04e2c264af
Lexy Perry <lexy.perry61022@gmail.com>
2025-07-08 22:31:31 -0400

    Created an app that displays different messages based on different data.

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+        
+    def get_size(self):
+        return self._size 
+    def set_size(self, value):
+        if value == "Small" or value == "Medium" or value == "Large":
+            self._size = value
+        else:
+            print("size must be Small, Medium, or Large")
+    size = property(get_size, set_size)
+
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price += 1    
